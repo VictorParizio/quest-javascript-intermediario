bd87afa52fc003d4d5452482db12fe2d87df8fae
Victor Parizio <victorparizio@hotmail.com>
2022-12-29 13:59:06 -0300

    Adicionado validação com JavaScript

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const btnEnviar = document.querySelector('.botao')
+const campo = document.querySelectorAll('[required]')
+const required = document.querySelectorAll('span')
+
+btnEnviar.addEventListener('click', () => {
+    campo.forEach((item, indice) => {
+            if (item.value === '') {
+                item.classList.remove('valido')
+                item.classList.add('invalido')
+                required[indice].classList.add('obrigatorio')
+            } else if (item.value !== '') {
+                item.classList.add('valido')
+                item.classList.remove('invalido')
+                required[indice].classList.remove('obrigatorio')
+            }
+        })
+    })
